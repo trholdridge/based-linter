eb523c1bdd867d85ce949e347c8aee4dc4e39337
trholdridge <trholdridge@gmail.com>
2022-11-19 16:01:26 -0500

    Added support for more filepaths

--- inout.py
+++ inout.py
@@ -21,11 +21,16 @@ def parse_and_warn():
 
 def read_files():
     ''' Function to return list of .py files in the current directory '''
-    return [file for file in os.listdir(sys.argv[1]) if '.py' == file[-3:]]
+    path = sys.argv[1]
+    if os.path.isfile(path):
+        files = [os.path.abspath(path)]
+    elif os.path.isdir(path):
+        files = [os.path.join(os.path.abspath(path), file) for file in os.listdir(path)]
+    return files
 
 def get_contents(file):
     ''' Read in 1 file and return its contents as a list of string '''
-    with open(os.path.join(sys.argv[1], file), 'r') as infile:
+    with open(file, 'r') as infile:
         return infile.readlines()
 
 def printWarnings(warn_list, fname):
@@ -37,4 +42,4 @@ def printWarnings(warn_list, fname):
               "libraries":"\033[1;32m" }
     for error in warn_list:
         print("File ", fname, ", line ", error.lineNumber, sep='')
-        print('\t', colors[error.type], error.type, ": ", error.warningMessage, END, sep='')
+        print('\t', colors[error.type], error.type, END, ": ", error.warningMessage, sep='')
