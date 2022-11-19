742007a22909b61ded65696d54899084938abf59
Victoria <72632385+victoria-adebiyi@users.noreply.github.com>
2022-11-19 12:05:43 -0500

    Add things in file

---
+++ warnings.py
@@ -0,0 +1,22 @@
+import collections
+
+biasWarning = collections.namedtuple('BiasWarning', ['lineNumber', 'type', 'warningMessage'])
+
+
+# Strings, Variable, Class names
+# Gender Associated Language
+# he/him/his, she/her/hers
+warnings = {
+    'BLUE_HAIR'
+    'PRONOUNS'
+}
+
+types = {
+    'GEN'
+}
+# Errors
+
+
+# Warnings
+finalDict = {
+}
